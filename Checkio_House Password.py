7e3c7567abd420d1a2cbb3f97b40bc5aa2f9785c
ClayLu <luehihi@gmail.com>
2018-08-19 21:17:59 +0800

    Add files via upload

---
+++ Checkio_House Password.py
@@ -0,0 +1,35 @@
+#https://py.checkio.org/station/home/
+
+import re
+
+def checkio(data):    
+    if len(data)>9 :
+        if re.match(".*[A-Z]+.*", data):
+            if re.match(".*[a-z]+.*", data):    
+                #print(re.match(".*[A-Z]+.*", data)  ) 
+                if re.match(".*[0-9]+.*", data):
+                    return   True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False            
+    else:
+        return False
+    #replace this for solution
+    #return True or False
+
+#Some hints
+#Just check all conditions
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
